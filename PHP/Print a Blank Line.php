8e7142b25eda9e1de8ec9874a914acb4a8390135
Riya Halbhavi <riyahal.dev@gmail.com>
2025-01-14 11:58:53 +0530

    Create Print a Blank Line.php

---
+++ PHP/Print a Blank Line.php
@@ -0,0 +1,16 @@
+// Print a blank line
+
+<?php
+	echo "Hello, World!";
+	// print a blank line
+	echo "\r\n";
+	echo "Hello, World!";
+	echo "\r\n";
+	echo "\r\n";
+	echo "Hello, World!";
+	echo "\r\n";
+	echo "\r\n";
+	echo "\r\n";
+	echo "Hello, World!";
+	echo "Hello, World!"
+?>
